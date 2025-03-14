b76315ed9f6a3549540d1560edd9e2667a994057
beeleza <higorbeleza.dev@gmail.com>
2025-03-13 23:10:49 -0300

    finalizando featuredposts component

--- src/components/FeaturedPosts.jsx
+++ src/components/FeaturedPosts.jsx
@@ -19,9 +19,60 @@ const FeaturedPosts = () => {
 
             {/* others */}
             <div className="w-full lg:w/12 flex flex-col gap-4">
-                <div className="lg:h-1/3 flex justify-between gap-4"></div>
-                <div className="lg:h-1/3 flex justify-between gap-4"></div>
-                <div className="lg:h-1/3 flex justify-between gap-4"></div>
+                <div className="lg:h-1/3 flex justify-between gap-4">
+                    <Image 
+                        src="featured2.jpeg"
+                        className="rounded-3xl object-cover w-1/3 aspect-video"    
+                    />
+
+                    <div className="w-2/3">
+                        <div className="flex items-center gap-4 text-sm lg:text-base mb-4">
+                            <h1 className="font-semibold">02.</h1>
+                            <Link className="text-blue-800">Web design</Link>
+                            <span className="text-gray-500 text-sm">2 days ago</span>
+                        </div>
+
+                        <Link to="/test" className="text-base sm:text-lg md:text-2xl lg:text-xl xl:text-2xl font-medium">
+                            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatum, nostrum!
+                        </Link>
+                    </div>
+                </div>
+                <div className="lg:h-1/3 flex justify-between gap-4">
+                    <Image 
+                        src="featured3.jpeg"
+                        className="rounded-3xl object-cover w-1/3 aspect-video"    
+                    />
+
+                    <div className="w-2/3">
+                        <div className="flex items-center gap-4 text-sm lg:text-base mb-4">
+                            <h1 className="font-semibold">03.</h1>
+                            <Link className="text-blue-800">Web design</Link>
+                            <span className="text-gray-500 text-sm">2 days ago</span>
+                        </div>
+
+                        <Link to="/test" className="text-base sm:text-lg md:text-2xl lg:text-xl xl:text-2xl font-medium">
+                            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatum, nostrum!
+                        </Link>
+                    </div>
+                </div>
+                <div className="lg:h-1/3 flex justify-between gap-4">
+                    <Image 
+                        src="featured4.jpeg"
+                        className="rounded-3xl object-cover w-1/3 aspect-video"    
+                    />
+
+                    <div className="w-2/3">
+                        <div className="flex items-center gap-4 text-sm lg:text-base mb-4">
+                            <h1 className="font-semibold">04.</h1>
+                            <Link className="text-blue-800">Web design</Link>
+                            <span className="text-gray-500 text-sm">2 days ago</span>
+                        </div>
+
+                        <Link to="/test" className="text-base sm:text-lg md:text-2xl lg:text-xl xl:text-2xl font-medium">
+                            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatum, nostrum!
+                        </Link>
+                    </div>
+                </div>
             </div>
         </div>
     )
